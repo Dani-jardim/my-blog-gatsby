7de2b8c90ee542a5f73aec0c548de964e1015de8
Dani-jardim <danialvesdesigner@gmail.com>
2021-01-15 16:09:12 -0300

    criando component MenuBar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Sidebar from "../Sidebar"
+import MenuBar from "../MenuBar"
 
 import * as S from './styled'
 import GlobalStyles from '../../styles/global'
-import Sidebar from "../Sidebar"
 
 
 const Layout = ({ children }) => {
@@ -13,6 +14,7 @@ const Layout = ({ children }) => {
       <GlobalStyles/>
       <Sidebar />
       <S.LayoutMain>{children}</S.LayoutMain>
+      <MenuBar />
     </S.LayoutWrapper>
 
   )
