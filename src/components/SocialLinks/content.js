b473c12610ede36d67debb4b398400723b253060
Dani-jardim <danialvesdesigner@gmail.com>
2021-01-15 15:18:23 -0300

    alterando redes sociais

--- src/components/SocialLinks/content.js
+++ src/components/SocialLinks/content.js
@@ -3,19 +3,19 @@ const handle = "Daniela"
 const links = [
   {
     label: "Github",
-    url: `https://github.com/${handle}`,
+    url: "https://github.com/Dani-jardim",
   },
   {
     label: "Twitter",
-    url: `https://twitter.com/${handle}`,
+    url: "https://twitter.com/danijardim00",
   },
   {
     label: "Instagram",
-    url: `https://www.instagram.com/${handle}`,
+    url: "https://www.instagram.com/danijardim00/",
   },
   {
     label: "Linkedin",
-    url: `https://linkedin.com/${handle}`,
+    url: "https://www.linkedin.com/in/daniela-jardim/",
   },
 ]
 
